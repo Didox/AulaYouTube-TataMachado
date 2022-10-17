f731f494147dab8b1668ea406c81b8a251a40abc
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-10-17 09:48:20 -0300

    correção api

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,23 +1,15 @@
-﻿using AulaYouTube.Models;
-using Microsoft.AspNetCore.Components;
+using AulaYouTube.Models;
 using Microsoft.AspNetCore.Mvc;
-using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
 
 namespace AulaYouTube.Controllers
 {
-
-
-    [Route("/")]
     [ApiController]
     public class HomeController : ControllerBase
     {
         [HttpGet]
-        public Apresentacao index()
-        {
+        [Route("/")]
+        public Apresentacao Index(){
             return new Apresentacao();
         }
-
     }
-
 }
-
